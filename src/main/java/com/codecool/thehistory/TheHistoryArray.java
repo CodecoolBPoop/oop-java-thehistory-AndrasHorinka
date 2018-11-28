05b33884392e9f1a5db4426f7d1d26a9762df0a7
AndrasHorinka <horinka.andras@gmail.com>
2018-11-28 17:25:49 +0100

    removeWord without List

--- src/main/java/com/codecool/thehistory/TheHistoryArray.java
+++ src/main/java/com/codecool/thehistory/TheHistoryArray.java
@@ -20,12 +20,37 @@ public class TheHistoryArray implements TheHistory {
     @Override
     public void removeWord(String wordToBeRemoved) {
         List<String> updatedList = Arrays.asList(String, wordsArray);
-        updatedList.removeAll(Arrays.asList("a"));
+        updatedList.removeAll(Arrays.asList(wordToBeRemoved));
         this.wordsArray = updatedList.toArray(new String[0]);
+
+        // alternative method without Lists
+//        int nextIndexOfSearchString = wordsArray.length;
+//        int counter = 0;
+//
+//        while (nextIndexOfSearchString >= 0) {
+//            nextIndexOfSearchString = Arrays.binarySearch(this.wordsArray, wordToBeRemoved);
+//            counter++;
+//            this.wordsArray[nextIndexOfSearchString] = "";
+//        }
+//
+//        String[] updatedWords = new String[this.wordsArray.length - counter];
+//        int nextIndexOfEmpty = 0;
+//        int firstNullIndexInDestination = 0;
+//        int lastIndexOfDestination = 0;
+//        while (nextIndexOfEmpty >= 0) {
+//            nextIndexOfEmpty = Arrays.binarySearch(this.wordsArray, firstNullIndexInDestination, this.wordsArray.length-1, "");
+//            System.arraycopy(this.wordsArray, firstNullIndexInDestination, updatedWords, lastIndexOfDestination, nextIndexOfEmpty - firstNullIndexInDestination);
+//            lastIndexOfDestination = nextIndexOfEmpty - firstNullIndexInDestination;
+//            firstNullIndexInDestination = Arrays.binarySearch(updatedWords, "");
+//        }
+//
+//        this.wordsArray = new String[updatedWords.length];
+//        System.arraycopy(updatedWords, 0, this.wordsArray, 0, updatedWords.length);
     }
 
     @Override
     public int size() {
+
         return this.wordsArray.length;
     }
 
@@ -36,12 +61,26 @@ public class TheHistoryArray implements TheHistory {
 
     @Override
     public void replaceOneWord(String from, String to) {
-        //TODO: check the TheHistory interface for more information
+        Arrays.sort(wordsArray);
+        int nextIndex = wordsArray.length;
+        while (nextIndex >= 0) {
+            nextIndex = Arrays.binarySearch(this.wordsArray, from);
+            this.wordsArray[nextIndex] = to;
+        }
     }
 
     @Override
     public void replaceMoreWords(String[] fromWords, String[] toWords) {
         //TODO: check the TheHistory interface for more information
+
+//        /**
+//         * Replaces all occurrences of a sentence or part of a sentence with another (part of a) sentence.
+//         * The order of words are important. Also the 'fromWords' and 'toWords' arrays are not necessarily same sized.
+//         * NOTE: replace() method uses this method!
+//         *
+//         * @param fromWords: array of words what should be replaced
+//         * @param toWords:   array of words which should replace the words of 'fromWords'
+//         */
     }
 
     @Override
