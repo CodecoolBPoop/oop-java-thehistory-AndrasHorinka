6e2f4c0829524423a3f17742ceab6d8d260b10a0
AndrasHorinka <horinka.andras@gmail.com>
2018-11-29 09:37:28 +0100

    clear

--- src/main/java/com/codecool/thehistory/TheHistoryLinkedList.java
+++ src/main/java/com/codecool/thehistory/TheHistoryLinkedList.java
@@ -29,7 +29,7 @@ public class TheHistoryLinkedList implements TheHistory {
 
     @Override
     public void clear() {
-        //TODO: check the TheHistory interface for more information
+        this.wordsLinkedList.clear();
     }
 
     @Override
